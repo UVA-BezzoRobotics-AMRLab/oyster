d55775bf5420c6d02bd1a3412d7451c7e6ebfc46
wangrui04 <wangruielena@gmail.com>
2026-05-02 22:12:31 -0400

    fixed installation dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name="oyster",
     version="0.1.0",
     packages=find_packages(),
+<<<<<<< Updated upstream
     install_requires=["numpy", "torch", "matplotlib", "gymnasium", "joblib"],
+=======
+    install_requires=["numpy", "torch", "matplotlib", "click", "gtimer", "joblib", "gym"],
+>>>>>>> Stashed changes
 )
