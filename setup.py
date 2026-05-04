cf3b86a2e423b34f94ae910e172c1eb2c33c0915
Ubuntu <nm9ur@virginia.edu>
2026-05-04 21:28:48 +0000

    fixed merge prob

--- setup.py
+++ setup.py
@@ -4,9 +4,5 @@ setup(
     name="oyster",
     version="0.1.0",
     packages=find_packages(),
-<<<<<<< Updated upstream
-    install_requires=["numpy", "torch", "matplotlib", "gymnasium", "joblib"],
-=======
-    install_requires=["numpy", "torch", "matplotlib", "click", "gtimer", "joblib", "gym"],
->>>>>>> Stashed changes
+    install_requires=["numpy", "torch", "matplotlib", "click", "gtimer", "joblib", "gym", "gymnasium"],
 )
